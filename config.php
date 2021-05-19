0d182767484a3333243addd445209caf9af6c823
Tors-0 <73916067+Tors-0@users.noreply.github.com>
2021-05-18 17:16:39 -0700

    Update config.php

--- config.php
+++ config.php
@@ -10,13 +10,13 @@ $webhook_url = ''; // Webhook url, guide https://support.discordapp.com/hc/en-us
 // I'd delete everything below if your using a custom form.
 
 // Form Configuration
-$browers_title = 'Discord Form - Demo'; // Title what appears in browers tab.
-$form_title = 'Discord Form Demo'; // Title inside of form.
+//$browers_title = 'Discord Form - Demo'; // Title what appears in browers tab.
+//$form_title = 'Discord Form Demo'; // Title inside of form.
 
 // Form Theming
-$background_color = '#236fb2'; // Color of background.
-$form_background_color = '#FFFFFF'; // Color of form background.
-$font_color = '#000000'; // Color of font.
-$form_input_color = '#FFFFFF'; // Color of form inputs.
-$buttom_type = 'btn-primary'; // Bootstrap button tags, https://getbootstrap.com/docs/4.0/components/buttons/
+//$background_color = '#236fb2'; // Color of background.
+//$form_background_color = '#FFFFFF'; // Color of form background.
+//$font_color = '#000000'; // Color of font.
+//$form_input_color = '#FFFFFF'; // Color of form inputs.
+//$buttom_type = 'btn-primary'; // Bootstrap button tags, https://getbootstrap.com/docs/4.0/components/buttons/
 ?>
