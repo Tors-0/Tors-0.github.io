e34ab1023e8f6de442054d44e3078a6e4ff1ffa1
Tors-0 <73916067+Tors-0@users.noreply.github.com>
2021-06-03 23:53:02 -0700

    Update chat.php

--- chat.php
+++ chat.php
@@ -10,7 +10,7 @@
     <body>
         <div id="wrapper">
             <div id="menu">
-                <p class="welcome">Welcome, <b></b></p>
+                <p class="welcome">Welcome, <b><?php echo $_SESSION['name']; ?></b></p>
                 <p class="logout"><a id="exit" href="#">Exit Chat</a></p>
             </div>
  
@@ -26,5 +26,94 @@
             // jQuery Document
             $(document).ready(function () {});
         </script>
+        <script type="text/javascript">
+// jQuery Document
+$(document).ready(function(){
+    //If user wants to end session
+    $("#exit").click(function(){
+        var exit = confirm("Are you sure you want to end the session?");
+        if(exit==true){window.location = 'index.php?logout=true';}      
+    });
+});
+            //If user submits the form
+$("#submitmsg").click(function () {
+    var clientmsg = $("#usermsg").val();
+    $.post("post.php", { text: clientmsg });
+    $("#usermsg").val("");
+    return false;
+});
+</script>
     </body>
+    <?php
+    if(isset($_GET['logout'])){ 
+     
+    //Simple exit message
+    $logout_message = "<div class='msgln'><span class='left-info'>User <b class='user-name-left'>". $_SESSION['name'] ."</b> has left the chat session.</span><br></div>";
+    file_put_contents("log.html", $logout_message, FILE_APPEND | LOCK_EX);
+     
+    session_destroy();
+    header("Location: index.php"); //Redirect the user
+}
+    ?>
+    <?php
+session_start();
+ 
+if(isset($_POST['enter'])){
+    if($_POST['name'] != ""){
+        $_SESSION['name'] = stripslashes(htmlspecialchars($_POST['name']));
+    }
+    else{
+        echo '<span class="error">Please type in a name</span>';
+    }
+}
+ 
+function loginForm(){
+    echo'
+    <div id="loginform">
+      <p>Please enter your name to continue!</p>
+      <form action="index.php" method="post">
+        <label for="name">Name &mdash;</label>
+        <input type="text" name="name" id="name" />
+        <input type="submit" name="enter" id="enter" value="Enter" />
+      </form>
+    </div>
+    ';
+}
+?>
+    <?php
+if(!isset($_SESSION['name'])){
+    loginForm();
+}
+else{
+?>
+<div id="wrapper">
+    <div id="menu">
+        <p class="welcome">Welcome, <b><?php echo $_SESSION['name']; ?></b></p>
+        <p class="logout"><a id="exit" href="#">Exit Chat</a></p>
+    </div>
+ 
+    <div id="chatbox">
+    <?php
+    if(file_exists("log.html") && filesize("log.html") > 0){
+         
+        $contents = file_get_contents("log.html");
+        echo $contents;
+    }
+    ?>
+    </div>
+ 
+    <form name="message" action="">
+        <input name="usermsg" type="text" id="usermsg" />
+        <input name="submitmsg" type="submit" id="submitmsg" value="Send" />
+    </form>
+</div>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script type="text/javascript">
+// jQuery Document
+$(document).ready(function(){
+});
+</script>
+<?php
+}
+?>
 </html>
